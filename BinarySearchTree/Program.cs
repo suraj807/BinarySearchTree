f90a3d60677440402ecca004213813186bc6c016
suraj807 <surajdev805@gmail.com>
2022-06-01 19:20:41 +0530

    CreateBSTWithMoreValues

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -10,14 +10,26 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-            Node<int> root = new Node<int>(56);
-            BinarySearchTree<int> bst = new BinarySearchTree<int>(root);
-            bst.Insert(30);
-            bst.Insert(70);
+            Node<int> root = new Node<int>(56); // Creating a root node having data as 56
+            BinarySearchTree<int> bst = new BinarySearchTree<int>(root); // Creating object of BinarySearchTree Generic class 
+            //calling insert method to add all the element
+            bst.Insert(root, 30);
+            bst.Insert(root, 70);
+            bst.Insert(root, 22);
+            bst.Insert(root, 40);
+            bst.Insert(root, 60);
+            bst.Insert(root, 95);
+            bst.Insert(root, 11);
+            bst.Insert(root, 65);
+            bst.Insert(root, 3);
+            bst.Insert(root, 16);
+            bst.Insert(root, 63);
+            bst.Insert(root, 67);
             Console.WriteLine("Inorder Traversal of given binary tree is : ");
-            bst.InOrderTraversal(root);
-            bst.TreeSize();
+            bst.InOrderTraversal(root); // Calling a method to display tree element in inorder
+            bst.TreeSize(); // Calling a method to display tree size
             Console.ReadLine();
         }
     }
 }
+  
